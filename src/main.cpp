090a74750d359932438af0c8873973da7942deb3
MePipe <mepipeofficial@gmail.com>
2026-06-09 17:59:24 -0400

    I finally feel okay removing the old main function

--- src/main.cpp
+++ src/main.cpp
@@ -19,192 +19,6 @@
 void ScanLibrary(LibraryHandler **lib, const char *path);
 void AddQueueButton(SDL_Renderer *renderer, Frame *frame, const char *trackName, unsigned int index);
 
-#ifndef NEW_MAIN
-int main(int argc, char* argv[]) {
-    SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_NAME_STRING, "MePlayer");
-    SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_VERSION_STRING, VERSION);
-    SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_IDENTIFIER_STRING, "not.mepipe.meplayer");
-    SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_CREATOR_STRING, "Not MePipe");
-    SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_URL_STRING, "https://github.com/NotMePipe/MePlayer");
-    SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_TYPE_STRING, "mediaplayer");
-
-#ifdef NDEBUG
-    av_log_set_level(AV_LOG_QUIET);
-#endif
-
-    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS))
-    {
-        std::cerr << "Failed to initialize SDL: " << SDL_GetError() << "\n";
-        return -1;
-    }
-
-    SDL_Window *window = SDL_CreateWindow("MePlayer", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    if (!window)
-    {
-        std::cerr << "Failed to create SDL window: " << SDL_GetError() << "\n";
-        SDL_Quit();
-        return -1;
-    }
-
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
-    if (!renderer)
-    {
-        std::cerr << "Failed to create SDL renderer: " << SDL_GetError() << "\n";
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
-
-    if (!TTF_Init()) {
-        std::cerr << "Failed to initialize SDL_ttf: " << SDL_GetError() << "\n";
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
-
-    bool quit = false;
-    SDL_Event event;
-
-    auto *queue = PlaybackQueue::GetPlaybackQueue();
-
-    bool debounce = false;
-
-    auto *lib = LibraryHandler::GetLibraryHandler();
-
-    ScanLibrary(&lib, "Music Folder");
-    lib->CreateButtons(renderer);
-    queue->Enqueue("1-hour-and-20-minutes-of-silence.mp3");
-
-    Track *playback;
-    queue->Play(&playback, 0);
-
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, playback->CoverArt());
-
-    while (!quit) {
-        if (SDL_GetKeyboardState(nullptr)[SDL_SCANCODE_SPACE]) {
-            if (!debounce) {
-                debounce = true;
-                if (playback->IsPaused()) {
-                    playback->Play();
-                } else {
-                    playback->Pause();
-                }
-            }
-        } else if (SDL_GetKeyboardState(nullptr)[SDL_SCANCODE_ESCAPE]) {
-            if (!debounce) {
-                debounce = true;
-                playback->Restart();
-            }
-        } else if (SDL_GetKeyboardState(nullptr)[SDL_SCANCODE_RIGHT]) {
-            if (!debounce) {
-                debounce = true;
-                playback->Jump(5);
-            }
-        } else if (SDL_GetKeyboardState(nullptr)[SDL_SCANCODE_LEFT]) {
-            if (!debounce) {
-                debounce = true;
-                playback->Jump(-5);
-            }
-        } else if (SDL_GetKeyboardState(nullptr)[SDL_SCANCODE_RETURN]) {
-            if (!debounce) {
-                debounce = true;
-                playback->Jump(60);
-            }
-        } else if (SDL_GetKeyboardState(nullptr)[SDL_SCANCODE_KP_ENTER]) {
-            if (!debounce) {
-                debounce = true;
-                playback->Jump(600);
-            }
-        } else if (SDL_GetKeyboardState(nullptr)[SDL_SCANCODE_BACKSPACE]) {
-            if (!debounce) {
-                debounce = true;
-                if (queue->Repeat() < 0) {
-                    queue->Repeat(static_cast<unsigned int>(queue->GetCurrentIndex()));
-                } else {
-                    queue->Repeat(false);
-                }
-            }
-        } else if (SDL_GetKeyboardState(nullptr)[SDL_SCANCODE_DELETE]) {
-            if (!debounce) {
-                debounce = true;
-                if (queue->Repeat() > -2) {
-                    queue->Repeat(true);
-                } else {
-                    queue->Repeat(false);
-                }
-            }
-        } else {
-            debounce = false;
-        }
-
-        while (SDL_PollEvent(&event)) {
-            switch (event.type)
-            {
-                case SDL_EVENT_QUIT:
-                    quit = true;
-                    break;
-                case SDL_EVENT_MOUSE_MOTION:
-                    for (const auto & button : lib->GetButtons()) {
-                        button->Hover(event.motion.x, event.motion.y);
-                    }
-                    break;
-                case SDL_EVENT_MOUSE_BUTTON_DOWN:
-                    if (event.button.button == SDL_BUTTON_LEFT) {
-                        for (const auto & button : lib->GetButtons()) {
-                            button->OnClick(event.button.x, event.button.y);
-                        }
-                    }
-                    break;
-                default: ;
-            }
-        }
-
-        if (playback->TrackEnded()) {
-            if (queue->Next(&playback) < 0) { // TODO Remove/reimplement temporary crash prevention
-                continue;
-            }
-            SDL_DestroyTexture(texture);
-            texture = SDL_CreateTextureFromSurface(renderer, playback->CoverArt());
-        }
-
-        std::string windowTitle = std::string(playback->Title())
-                                    .append(" (")
-                                    .append(playback->Artist())
-                                    .append(") : ")
-                                    .append(playback->GetPlaybackPosition())
-                                    .append("/")
-                                    .append(playback->GetTrackLength());
-
-        SDL_SetWindowTitle(window, windowTitle.c_str());
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-
-        SDL_FRect dst = {(static_cast<float>(SCREEN_WIDTH) / 2) - 150, (static_cast<float>(SCREEN_HEIGHT) / 2) - 150, 300, 300};
-
-        SDL_RenderTexture(renderer, texture, nullptr, &dst);
-
-        lib->Render(renderer);
-
-        SDL_RenderPresent(renderer);
-    }
-
-    PlaybackQueue::Close();
-
-    SDL_DestroyTexture(texture);
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-
-    TTF_Quit();
-    SDL_Quit();
-
-    LibraryHandler::Close();
-
-    return 0;
-}
-#else
 int main(int argc, char* argv[]) {
     SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_NAME_STRING, "MePlayer");
     SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_VERSION_STRING, VERSION);
@@ -279,6 +93,7 @@ int main(int argc, char* argv[]) {
                     break;
                 case SDL_EVENT_MOUSE_WHEEL:
                     songSelect->Scroll(event.wheel.mouse_x, event.wheel.mouse_y, event.wheel.y * SCROLL_SCALE);
+                    queueFrame->Scroll(event.wheel.mouse_x, event.wheel.mouse_y, event.wheel.y * SCROLL_SCALE);
                     break;
                 default:
                     songSelect->HandleEvent(event);
@@ -323,7 +138,6 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-#endif
 
 void ScanLibrary(LibraryHandler **lib, const char *path) {
     SDL_PathInfo info;
