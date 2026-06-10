e37e0aeeefd2726b13c2ff244c9a145c89838e89
MePipe <mepipeofficial@gmail.com>
2026-06-10 17:59:06 -0400

    Fix scrolling direction

--- src/ScrollingFrame.cpp
+++ src/ScrollingFrame.cpp
@@ -7,7 +7,7 @@ ScrollingFrame::ScrollingFrame(const float x, const float y, const float w, cons
 void ScrollingFrame::Scroll(float x, float y, float amount) { // TODO Add bounds
     if (IsInBounds(x, y)) {
         for (const auto &button : buttons) {
-            button->SetPos(button->GetBorder().x, button->GetBorder().y - amount);
+            button->SetPos(button->GetBorder().x, button->GetBorder().y + amount);
         }
     }
 }
